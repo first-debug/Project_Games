c06b3e5a987899759bb7f953b32b082a606ca945
freed0m <nazar.garaev.07@mail.ru>
2022-01-21 10:43:49 +0500

    Прокачал начальный экран

--- Игра/Game.py
+++ Игра/Game.py
@@ -3,8 +3,9 @@ from os import path
 from data.board import Board
 
 
-SIZE = WIGHT, HEIGHT = 800, 600
+SIZE = WIDTH, HEIGHT = 800, 600
 FPS = 144
+BUTTON_COLOR = pygame.Color('gray')
 
 
 class Player(pygame.sprite.Sprite):
@@ -40,7 +41,7 @@ def draw_game_world():
     fon = load_image('fon.png')
     screen.blit(fon, (0, 0))
     #  отрисовываем землю
-    GRAUND = pygame.Rect(0, 400, WIGHT, 200)
+    GRAUND = pygame.Rect(0, 400, WIDTH, 200)
     pygame.draw.rect(screen, 'brown', GRAUND)
     #  рисуем персонажа
     pos_of_player = [320, 320]
@@ -49,55 +50,71 @@ def draw_game_world():
     return board, player_image, pos_of_player
 
 
-def start_screen():
-    intro_text = ["Mario 0.1", "", "", "", "", "", "", "", "",
-                  "", "", "Настройки", "", "", "Правила игры",
-                  "Чтобы продолжить нажмите Enter"]
+def rules_screen():
+    # здесь будет создаваться и отрисовываться экран правил
+    pass
 
-    fon = pygame.transform.scale(load_image('fon_start_screen.jpg'), (WIGHT, HEIGHT))
+
+def start_screen():
+    intro_text = ["Mario 0.1", 'Играть', 'Настройки', 'Правила игры']
+
+    fon = pygame.transform.scale(load_image('fon_start_screen.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    font_of_title = pygame.font.Font(None, 60)
-    text_coord = 50
+    font = pygame.font.Font(None, 40)
+    size_font_of_title = 60
+    font_of_title = pygame.font.Font(None, size_font_of_title)
+    text_coord = (HEIGHT // 3) - size_font_of_title
+    anim_color = pygame.Color((50, 50, 50))
+
     for line in intro_text:
         # разделим настройки шрифта для заголовка и подсказки
         if line != 'Mario 0.1':
             string_rendered = font.render(line, 1, pygame.Color('white'))
             string_rendered_shadow = font.render(line, 1, pygame.Color('black'))
             intro_rect = string_rendered.get_rect()
-            text_coord += 10
+            text_coord += 50
             intro_rect.top = text_coord
-            intro_rect.x = 10
+            intro_rect.x = (WIDTH - intro_rect.width) // 2
+            pygame.draw.rect(screen, BUTTON_COLOR,
+                             (intro_rect.x - 6, intro_rect.y - 6, intro_rect.width + 12, intro_rect.height + 12))
+            pygame.draw.rect(screen, anim_color, (intro_rect.x - 6, intro_rect.y - 6, intro_rect.width + 12, intro_rect.height + 12), 3)
         else:
             string_rendered = font_of_title.render(line, 1, pygame.Color('white'))
             string_rendered_shadow = font_of_title.render(line, 1, pygame.Color('black'))
             intro_rect = string_rendered.get_rect()
             text_coord += 10
             intro_rect.top = text_coord
-            intro_rect.x = 10
+            intro_rect.x = (WIDTH - intro_rect.width) // 2
         text_coord += intro_rect.height
         screen.blit(string_rendered_shadow, intro_rect.move(1, 1))
         screen.blit(string_rendered, intro_rect)
-        #  находим координаты верхнего левого угла кнопки 'Настройки'
+        #  находим координаты верхнего левого угла кнопок
         #  в будущем может понадобиться
         print(intro_rect.topleft, line == 'Настройки')
         if line == 'Настройки':
             settings_rect = intro_rect
+        elif line == 'Играть':
+            play_rect = intro_rect
+        elif line == 'Правила игры':
+            rules_rect = intro_rect
     pygame.display.flip()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
-                    print('Перешли к игре')
-                    return
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    touching = settings_rect.collidepoint(event.pos)
-                    if touching:
+                    touching_settings = settings_rect.collidepoint(event.pos)
+                    touching_play = play_rect.collidepoint(event.pos)
+                    touching_rules = rules_rect.collidepoint(event.pos)
+                    if touching_settings:
                         settings_screen()
                         print('Перешли в настройки')
+                    elif touching_play:
+                        return
+                    elif touching_rules:
+                        rules_screen()
+                        print('Перешли в правила игры')
         clock.tick(FPS)
 
 
